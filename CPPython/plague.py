de77869066344032bbbb2a37f3939bc03139fee1
Muhammad Musab Ali Khan <136508816+musab285@users.noreply.github.com>
2025-07-08 23:15:52 +0000

    sheep.cpp sortperm.cpp

---
+++ CPPython/plague.py
@@ -0,0 +1,17 @@
+t= int(input())
+for x in range(t):
+    n, m, l, r = map(int, input().split())
+    tm = n-m
+    # ls = 0
+    # rs = 0
+    if l+tm<=0:
+        l+=tm
+    elif r-tm>=0:
+        r-=tm
+    else:
+        tm += l
+        l=0
+        r-=tm
+        
+            
+    print(l, r)
